76db049d29c359270b1212e9389f687c1cdd4d3a
uhfm <uhfm@fresnou.org>
2022-10-17 11:33:34 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', async function(req, res, next) {
+  const {sequelize} = require("../models/index")
+  const {QueryTypes} = require("sequelize")
+
+  let toDoItems = await sequelize.query('select * from todo', {type: QueryTypes.SELECT})
+  res.render('index', {toDoItems})
 });
 
 module.exports = router;
