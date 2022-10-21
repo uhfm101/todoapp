4950171078804b97a39dd85c43b6fa9a66891b82
uhfm <uhfm@fresnou.org>
2022-10-21 11:18:04 -0700

    commit 4

---
+++ controllers/todo_controller.js
@@ -0,0 +1,65 @@
+const {sequelize} = require("../models/index");
+const {QueryTypes} = require("sequelize");
+const {Todo} = require('../models/index')
+
+module.exports.homeRoute = async function(req,res,next){
+    let toDoItems = await Todo.findAll();
+    res.render("index", {toDoItems});
+}
+
+module.exports.renderAddForm = function(req,res){
+    res.render('create_todo');
+};
+
+module.exports.addNewItem = async function(req,res){
+    await Todo.create({
+            description: req.body.description
+    })
+    res.redirect("/")
+};
+
+module.exports.markItemAsComplete = async function(req,res){
+    await Todo.update( {completed: true}, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect("/")
+};
+
+module.exports.markItemAsIncomplete = async function(req,res){
+    await Todo.update( {completed: false}, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect("/")
+};
+
+module.exports.deleteItem = async function(req, res){
+    await Todo.destroy({
+        Where: {
+            id: req.params.id
+        }
+    })
+    res.redirect('/')
+};
+
+module.exports.renderEditForm = async function(req, res){
+    let todo = await Todo.findByPk(req.params.id);
+    res.render('edit_todo', {
+        item: {
+            description: todo.description,
+                id: todo.id
+        }
+    })
+};
+
+module.exports.updateItem = async function(req, res){
+    await Todo.update( {description: req.body.description},{
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect("/")
+};
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ router.get('/add', toDoController.renderAddForm);
 
 router.post('/add', toDoController.addNewItem);
 
-
 router.get('/complete/:id',toDoController.markItemAsComplete);
 
 router.get('/incomplete/:id', toDoController.markItemAsIncomplete);
@@ -18,6 +17,6 @@ router.get('/delete/:id', toDoController.deleteItem);
 
 router.get(`/edit/:id`, toDoController.renderEditForm);
 
-router.post('/edit/:id', toDoController.editItem);
+router.post('/edit/:id', toDoController.updateItem);
 
 module.exports = router;
